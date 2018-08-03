49995c22658296632c18b6126923c61b67027bb0
Giselle <gigi108@gmail.com>
2018-08-03 15:27:34 +0000

    started styling

--- bookreview.py
+++ bookreview.py
@@ -10,26 +10,25 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
-@app.route("/")
+@app.route('/')
 def home_page():
-    return render_template('index.html', authors = mongo.db.authors.find())
+    return render_template('index.html')
     
 @app.route('/book_list')
 def book_list():
-    return render_template('booklist.html',  books = mongo.db.books.find())
+    books = mongo.db.books.find()
+    return render_template('booklist.html',  books = books)
 
+@app.route('/authors')
+def authors():
+    return render_template('authors.html', authors = mongo.db.authors.find())
 
-# @app.route('/book_list/<books_author>')
-# def books_by_author(author):
-#     if author == mongo.db.books.find()
-#     return render_template(booklist.html, )
-    
-    
     
 @app.route('/book_details/<books_id>')
 def book_details(books_id):
     the_book = mongo.db.books.find_one({"_id": ObjectId(books_id)})
-    return render_template('bookdetails.html',  book = the_book)
+    the_author = mongo.db.authors.find_one({"name": the_book['author']})
+    return render_template('bookdetails.html',  book = the_book, author = the_author)
 
 @app.route('/add_book', methods=['POST', 'GET'])
 def add_book():
@@ -38,7 +37,20 @@ def add_book():
         return redirect(url_for('book_details', books_id=new_book.inserted_id))
     else:
         return render_template('addbook.html')
-    
+        
+@app.route('/author_details/<author_id>')
+def author_details(author_id):
+    the_author = mongo.db.authors.find_one({"_id": ObjectId(author_id)})
+    books = mongo.db.books.find({'author': the_author['name']})
+    return render_template('authordetails.html', author=the_author, books = books)
+
+
+@app.route('/author_details_byname/<author_name>')
+def author_details_byname(author_name):
+    the_author = mongo.db.authors.find_one({"name": author_name})
+    books = mongo.db.books.find({'author': the_author['name']})
+    return render_template('authordetails.html', author=the_author, books = books)
+
 @app.route('/add_author', methods=['POST', 'GET'])
 def add_author():
     if request.method == 'POST':
